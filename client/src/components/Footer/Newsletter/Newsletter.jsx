6d91d327c9cbb04331b8922338d8e8abc8ac9754
saad-ahmed006 <muhammadsaad368444@gmail.com>
2023-07-05 17:09:30 +0500

    newsletter-component-add

--- client/src/components/Footer/Newsletter/Newsletter.jsx
+++ client/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,27 @@
+import React from "react";
 import "./Newsletter.scss";
+import { AiFillLinkedin, AiFillFacebook, AiFillTwitterSquare, AiFillInstagram } from 'react-icons/ai'
 const Newsletter = () => {
-    return <div>Newsletter</div>;
-};
+    return (
+    <div className="newsLetterContainer">
+
+        <div className="newsLetterContent">
+            <p>NewsLetter</p>
+            <h1>Sign Up For Lasted Updates and Offers</h1>
+            <div className="inputContainer">
+                <input type="email"></input>
+                <button className="btn">Subscribe</button>
+            </div>
+            <span>Will be used accordance with our privicy policy</span>
+            <div className="mediaIcons">
+                <span><AiFillLinkedin /></span>
+                <span><AiFillFacebook /></span>
+                <span><AiFillTwitterSquare /></span>
+                <span><AiFillInstagram /></span>
+            </div>
+        </div>
+    </div>
+    )
+}
 
 export default Newsletter;
