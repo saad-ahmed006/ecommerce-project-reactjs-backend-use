8ec45c596ea2fe173a4693024ccfbb25ef417a1a
saad-ahmed006 <muhammadsaad368444@gmail.com>
2023-07-05 15:29:41 +0500

    banner-component-add

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,8 +1,21 @@
+import React from "react";
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png"
 const Banner = () => {
-    return (
-        <div >Bannar</div>
-    )
+    return <div className="bannerSection">
+        <div className="content">
+            <div className="text-content">
+                <h1>SALES</h1>
+                <p>Convallis interium pures adipiscring dis parturient posuere as a quan a eleitend nontes parturient posuere curse terror</p>
+                <div className="ctas">
+                <div className="banner-ctas">Read More</div>
+                <div className="banner-ctas v2">Shop Now</div>
+
+                </div>
+            </div>
+            <img src={BannerImg} className="Image" alt="bannar"></img>
+        </div>
+    </div>;
 };
 
 export default Banner;
