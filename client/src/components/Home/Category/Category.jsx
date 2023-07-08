61da45928bea31fcdbf8368cf3991910a9d78b56
saad-ahmed006 <muhammadsaad368444@gmail.com>
2023-07-08 12:00:44 +0500

    category-component-add

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,6 +1,24 @@
 import "./Category.scss";
+import Img from '../../../assets/cat-1.jpg'
 const Category = () => {
-    return <div>Category</div>;
+    return (
+        <div className="shop-by-category">
+            <div className="categories">
+                <div className="category" >
+                    <img src={Img} />
+                </div>
+                <div className="category" >
+                    <img src={Img} />
+                </div>
+                <div className="category" >
+                    <img src={Img} />
+                </div>
+                <div className="category" >
+                    <img src={Img} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Category;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,26 @@
+import React, { useEffect } from "react";
 import "./Home.scss";
 import Banner from './Banner/Banner'
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from '../../utils/api'
 const Home = () => {
-    return(
-    <>
-    <div className="home"></div>
-        <Banner />
-        <Category />  
-        <Products innerHeading={true} headindText={'popular products'}/> 
-    </>
-        )
+    useEffect(() => {
+        fetchCategoriesData()
+    }, [])
+
+    const fetchCategoriesData = () => {
+        fetchDataFromApi("/api/products?populate=*").then(res => console.log(res))
+        
+    }
+    return (
+        <>
+            <div className="home" ></div>
+            <Banner />
+            <Category />
+            <Products innerHeading={true} headindText={'popular products'} />
+        </>
+    )
 };
 
 export default Home;
